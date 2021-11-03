bf8103da563e06d62497e7326f86eb12d5497609
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2021-11-03 21:52:19 +0100

    visimp

---
+++ zillertal-alps-italy_void-linux/.config/nvim/init.lua
@@ -0,0 +1,18 @@
+require'visimp'{
+  defaults = {
+    foldmethod = 'marker'
+  },
+  latex = {
+    tectonic = true
+  },
+  languages = {
+    'ampl',
+    'bash',
+    'c',
+    'latex', 
+    'python'
+  },
+  lspsignature = {},
+  outline = {},
+  theme = {'haishanh/night-owl.vim', 'night-owl', 'dark'}
+}
